15f4fb11adf840dac76f005e526f44988dfc094f
Daniel Stern <daniel@danielstern.ca>
2014-11-01 09:42:05 -0700

    styles-added

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -189,56 +189,316 @@ angular.module("DemoApp",['ui.router','colorPicker'])
 	    "namespace": ""
 	},
 	"Lorien":{
+	    "thumb-color": {
+	        "r": 67,
+	        "g": "150",
+	        "b": 67,
+	        "a": 1
+	    },
+	    "track-color": {
+	        "r": 32,
+	        "g": "89",
+	        "b": 40,
+	        "a": "1"
+	    },
+	    "thumb-border-color": {
+	        "r": 131,
+	        "g": 229,
+	        "b": "132",
+	        "a": 1
+	    },
+	    "thumb-radius": "12",
+	    "thumb-height": "23",
+	    "thumb-shadow-size": "2.6",
+	    "thumb-width": "23",
+	    "thumb-shadow-color": {
+	        "r": 0,
+	        "g": "170",
+	        "b": 0,
+	        "a": 1
+	    },
+	    "thumb-shadow-blur": "3.7",
+	    "thumb-border-width": "2.5",
+	    "track-shadow-size": "0.9",
+	    "track-shadow-blur": "1.7",
+	    "track-border-width": "1.1",
+	    "track-height": "11.3",
+	    "track-radius": "1",
+	    "track-border-color": {
+	        "r": "24",
+	        "g": 213,
+	        "b": 1,
+	        "a": 1
+	    },
+	    "track-shadow-color": {
+	        "r": 0,
+	        "g": "34",
+	        "b": 0,
+	        "a": 1
+	    },
+	    "contrast": 5,
+	    "namespace": ""
+	},
+	"Accessible":{
     "thumb-color": {
-        "r": 67,
-        "g": "150",
-        "b": 67,
-        "a": 1
-    },
-    "track-color": {
-        "r": 32,
-        "g": "89",
-        "b": 40,
+        "r": 48,
+        "g": 113,
+        "b": 169,
         "a": "1"
     },
-    "thumb-border-color": {
-        "r": 131,
-        "g": 229,
-        "b": "132",
+    "track-color": {
+        "r": 255,
+        "g": 255,
+        "b": "214",
         "a": 1
     },
-    "thumb-radius": "12",
-    "thumb-height": "23",
-    "thumb-shadow-size": "2.6",
-    "thumb-width": "23",
+    "thumb-border-color": {
+        "r": "0",
+        "g": 30,
+        "b": 0,
+        "a": "0.57"
+    },
+    "thumb-radius": "4",
+    "thumb-height": "32",
+    "thumb-shadow-size": "0.8",
+    "thumb-width": "32",
     "thumb-shadow-color": {
         "r": 0,
-        "g": "170",
-        "b": 0,
-        "a": 1
+        "g": 0,
+        "b": "62",
+        "a": "0.67"
     },
-    "thumb-shadow-blur": "3.7",
-    "thumb-border-width": "2.5",
-    "track-shadow-size": "0.9",
-    "track-shadow-blur": "1.7",
-    "track-border-width": "1.1",
-    "track-height": "11.3",
-    "track-radius": "1",
+    "thumb-shadow-blur": "1.9",
+    "thumb-border-width": "1.9",
+    "track-shadow-size": 1,
+    "track-shadow-blur": 1,
+    "track-border-width": "0",
+    "track-height": "32",
+    "track-radius": "0",
     "track-border-color": {
-        "r": "24",
-        "g": 213,
+        "r": 1,
+        "g": 1,
         "b": 1,
         "a": 1
     },
     "track-shadow-color": {
         "r": 0,
-        "g": "34",
+        "g": 0,
+        "b": 0,
+        "a": 1
+    },
+    "contrast": 5,
+    "namespace": "slider"
+},
+	"Cylon":{
+    "thumb-color": {
+        "r": 255,
+        "g": 55,
+        "b": "56",
+        "a": "0.93"
+    },
+    "track-color": {
+        "r": 70,
+        "g": "38",
+        "b": 44,
+        "a": 1
+    },
+    "thumb-border-color": {
+        "r": "148",
+        "g": 30,
+        "b": 0,
+        "a": 1
+    },
+    "thumb-radius": "28",
+    "thumb-height": "25",
+    "thumb-shadow-size": "1.8",
+    "thumb-width": "34",
+    "thumb-shadow-color": {
+        "r": "255",
+        "g": 0,
+        "b": 0,
+        "a": "0.49"
+    },
+    "thumb-shadow-blur": "5.9",
+    "thumb-border-width": "2.9",
+    "track-shadow-size": 1,
+    "track-shadow-blur": 1,
+    "track-border-width": "0",
+    "track-height": "31.2",
+    "track-radius": "0",
+    "track-border-color": {
+        "r": 1,
+        "g": 1,
+        "b": 1,
+        "a": 1
+    },
+    "track-shadow-color": {
+        "r": 0,
+        "g": 0,
+        "b": 0,
+        "a": 1
+    },
+    "contrast": 5,
+    "namespace": ""
+},
+	"BSOD":{
+    "thumb-color": {
+        "r": 255,
+        "g": 255,
+        "b": "255",
+        "a": "1"
+    },
+    "track-color": {
+        "r": 0,
+        "g": 0,
+        "b": "255",
+        "a": "1"
+    },
+    "thumb-border-color": {
+        "r": 0,
+        "g": 30,
+        "b": 0,
+        "a": "0.57"
+    },
+    "thumb-radius": "0",
+    "thumb-height": "32",
+    "thumb-shadow-size": "0",
+    "thumb-width": "17",
+    "thumb-shadow-color": {
+        "r": 0,
+        "g": 0,
+        "b": 62,
+        "a": "0.67"
+    },
+    "thumb-shadow-blur": "0",
+    "thumb-border-width": "0.4",
+    "track-shadow-size": "0.1",
+    "track-shadow-blur": "0",
+    "track-border-width": "0",
+    "track-height": "32",
+    "track-radius": "0",
+    "track-border-color": {
+        "r": 1,
+        "g": 1,
+        "b": 1,
+        "a": 1
+    },
+    "track-shadow-color": {
+        "r": 0,
+        "g": "89",
+        "b": 0,
+        "a": 1
+    },
+    "contrast": "25",
+    "namespace": ""
+},
+	"Magic Unicorn Journey":{
+    "thumb-color": {
+        "r": 255,
+        "g": 255,
+        "b": "125",
+        "a": 1
+    },
+    "track-color": {
+        "r": 255,
+        "g": "145",
+        "b": 216,
+        "a": "1"
+    },
+    "thumb-border-color": {
+        "r": 255,
+        "g": "205",
+        "b": 255,
+        "a": 1
+    },
+    "thumb-radius": "12",
+    "thumb-height": "30",
+    "thumb-shadow-size": "4.1",
+    "thumb-width": "30",
+    "thumb-shadow-color": {
+        "r": 39,
+        "g": 170,
+        "b": "255",
+        "a": 1
+    },
+    "thumb-shadow-blur": "7.2",
+    "thumb-border-width": "5",
+    "track-shadow-size": "1.4",
+    "track-shadow-blur": "1.7",
+    "track-border-width": "2.3",
+    "track-height": "13.3",
+    "track-radius": "25",
+    "track-border-color": {
+        "r": "219",
+        "g": 213,
+        "b": 143,
+        "a": 1
+    },
+    "track-shadow-color": {
+        "r": 255,
+        "g": 178,
+        "b": "223",
+        "a": 1
+    },
+    "contrast": "8",
+    "namespace": ""
+},
+	"Bootstrap 2":{
+    "thumb-color": {
+        "r": 255,
+        "g": 255,
+        "b": 255,
+        "a": 1
+    },
+    "track-color": {
+        "r": 48,
+        "g": 113,
+        "b": 169,
+        "a": "0.78"
+    },
+    "thumb-border-color": {
+        "r": 0,
+        "g": 0,
+        "b": "30",
+        "a": 1
+    },
+    "thumb-radius": "15",
+    "thumb-height": "26",
+    "thumb-shadow-size": "0.9",
+    "thumb-width": "26",
+    "thumb-shadow-color": {
+        "r": 0,
+        "g": 0,
+        "b": "49",
+        "a": 1
+    },
+    "thumb-shadow-blur": 1,
+    "thumb-border-width": "1.8",
+    "track-shadow-size": 1,
+    "track-shadow-blur": 1,
+    "track-border-width": "0.2",
+    "track-height": "11.4",
+    "track-radius": "1.3",
+    "track-border-color": {
+        "r": 1,
+        "g": 1,
+        "b": 1,
+        "a": 1
+    },
+    "track-shadow-color": {
+        "r": 0,
+        "g": 0,
         "b": 0,
         "a": 1
     },
     "contrast": 5,
     "namespace": ""
 }
+
+
+
+
+
 
 
 
