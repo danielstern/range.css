b4d5ef9cb9ecc3269eda526d5b210a643a0de253
Daniel Stern <daniel@danielstern.ca>
2014-11-01 08:31:46 -0700

    color-picker-working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -56,7 +56,7 @@ angular.module("DemoApp", ['ui.router']).config(function ($stateProvider, $urlRo
         'track-border-color': {
             r: 1,
             g: 1,
-            b: 0,
+            b: 1,
             a: 1
         },
         'track-shadow-color': {
@@ -74,6 +74,61 @@ angular.module("DemoApp", ['ui.router']).config(function ($stateProvider, $urlRo
         templateUrl: 'partials/color-picker.html',
         scope: {
             color: "="
+        },
+        controller: function ($scope) {
+            function hexstr(number) {
+                var chars = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
+                var low = number & 0xf;
+                var high = (number >> 4) & 0xf;
+                return "" + chars[high] + chars[low];
+            }
+
+            function rgbToHex(r, g, b) {
+                return hexstr(r) + hexstr(g) + hexstr(b);
+            }
+
+            function sixToThree(str) {
+                if (str[0] === str[1] && str[2] === str[3] && str[4] === str[5]) {
+                    return str[0] + str[2] + str[4];
+                } else {
+                    return str;
+                }
+            }
+
+            function hexToRgb(hex) {
+                var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+                hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+                    return r + r + g + g + b + b;
+                });
+
+                var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+                return result ? {
+                    r: parseInt(result[1], 16),
+                    g: parseInt(result[2], 16),
+                    b: parseInt(result[3], 16),
+                    a: $scope.alpha
+                } : null;
+            }
+
+            $scope.$watch('color', function (color) {
+                if (color) {
+                    $scope.alpha = color.a;
+                    $scope.hex = rgbToHex($scope.color.r, $scope.color.g, $scope.color.b);
+                    $scope.hex = "#" + sixToThree($scope.hex);
+                }
+            }, true);
+
+            $scope.$watch('hex', function (hex) {
+                if (!hex) {
+                    return;
+                }
+                try  {
+                    var color = hexToRgb(hex);
+                    $scope.color = color;
+                } catch (e) {
+                    console.warn("Invalid color");
+                }
+            });
         }
     };
 }).run(function ($rootScope, presets) {
--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -91,6 +91,67 @@ angular.module("DemoApp",['ui.router'])
 		templateUrl:'partials/color-picker.html',
 		scope: {
 			color:"="
+		},
+		controller: function($scope) {
+			
+			function hexstr(number) {
+			    var chars = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
+			    var low = number & 0xf;
+			    var high = (number >> 4) & 0xf;
+			    return "" + chars[high] + chars[low];
+			}
+
+			function rgbToHex(r, g, b) {
+			    return hexstr(r) + hexstr(g) + hexstr(b);
+			}
+
+			function sixToThree(str) {
+				if (str[0] === str[1] && str[2] === str[3] && str[4] === str[5]) {
+					return str[0] + str[2] + str[4];
+				} else {
+					return str;
+				}
+			}
+
+			function hexToRgb(hex) {
+			    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+			    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+			    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+			        return r + r + g + g + b + b;
+			    });
+
+			    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+			    return result ? {
+			        r: parseInt(result[1], 16),
+			        g: parseInt(result[2], 16),
+			        b: parseInt(result[3], 16),
+			        a:$scope.alpha
+			    } : null;
+			}
+
+
+			$scope.$watch('color',function(color){
+				if (color) {
+					$scope.alpha = color.a;
+					$scope.hex = rgbToHex($scope.color.r,$scope.color.g,$scope.color.b);
+					$scope.hex = "#" + sixToThree($scope.hex);
+				}
+				// if (color.fromInput) {
+					// return;
+				// }
+			},true);
+
+			$scope.$watch('hex',function(hex){
+				if (!hex) {
+					return;
+				}
+				try {
+					var color = hexToRgb(hex);
+					$scope.color = color;
+				} catch (e) {
+					console.warn("Invalid color");
+				}
+			})
 		}
 	}
 	})
